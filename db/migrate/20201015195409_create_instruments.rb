1bad9a9825a3c53620707706b6aca39dbbefc352
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-15 15:00:50 -0500

    generated Instrument model

---
+++ db/migrate/20201015195409_create_instruments.rb
@@ -0,0 +1,13 @@
+class CreateInstruments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :instruments do |t|
+      t.string :name
+      t.string :brand
+      t.string :category
+      t.string :type
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
