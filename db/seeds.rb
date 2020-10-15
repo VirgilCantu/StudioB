fd8d1c4876d076d3c9e210cfd84e1a663b4eb512
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-15 17:22:23 -0500

    added 5 users to seed data to be associated with artist role

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,52 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(
+    name: "Billie Eilish Pirate Baird O'Connell", 
+    email: "billie@billieeilish.com", 
+    password: "billieeilish", 
+    location: "Los Angeles, California", 
+    bio:,
+    can_travel: true, 
+    picture: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Billie_Eilish_2019_by_Glenn_Francis_%28cropped%29_2.jpg",
+    stage_name: "Billie Eilish" )
 
+User.create(
+    name: "Austin Richard Post", 
+    email: "austin@postmalone.com", 
+    password: "postmalone", 
+    location: "Los Angeles, California", 
+    bio:,
+    can_travel: false, 
+    picture: "https://upload.wikimedia.org/wikipedia/commons/1/12/Post_Malone_at_the_2019_American_Music_Awards.png",
+    stage_name: "Post Malone" )
+
+User.create(
+    name: "Melissa Viviane Jefferson", 
+    email: "melissa@lizzo.com", 
+    password: "lizzo", 
+    location: "Houston, Texas", 
+    bio:,
+    can_travel: true, 
+    picture: "https://upload.wikimedia.org/wikipedia/commons/9/9d/LizzoBrixt06Nov19-10_%2849216792848%29_%28cropped%29.jpg",
+    stage_name: "Lizzo" )
+
+User.create(
+    name: "Harry Edward Styles", 
+    email: "harry@harrystyles.com", 
+    password: "harrystyles", 
+    location: "London, England", 
+    bio:,
+    can_travel: false, 
+    picture: "https://upload.wikimedia.org/wikipedia/commons/9/9e/Harry_Styles_November_2014.jpg",
+    stage_name: "Harry Styles" )
+
+User.create(
+    name: "Aubrey Drake Graham", 
+    email: "aubrey@drake.com", 
+    password: "drake", 
+    location: "Toronto, Canada",
+    bio:,
+    can_travel: true, 
+    picture: "https://upload.wikimedia.org/wikipedia/commons/1/15/Drake_at_The_Carter_Effect_2017_%2836818935200%29_%28cropped%29.jpg",
+    stage_name: "Drake" )
