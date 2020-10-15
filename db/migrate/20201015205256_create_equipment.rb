bf9ca195efc81fa4507f2a35cf8294f95eebe019
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-15 15:55:13 -0500

    generated Equipment model

---
+++ db/migrate/20201015205256_create_equipment.rb
@@ -0,0 +1,13 @@
+class CreateEquipment < ActiveRecord::Migration[6.0]
+  def change
+    create_table :equipment do |t|
+      t.string :name
+      t.string :brand
+      t.string :category
+      t.string :type
+      t.belongs_to :studio, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
