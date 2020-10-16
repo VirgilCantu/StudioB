5b6c00a644a1545ff639db81b98514614fb10bb7
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-15 20:10:33 -0500

    added several genres to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -169,3 +169,22 @@ User.create(
     stage_name: "Billy Preston" )
 
 
+#----------------------Genres--------------------# 
+
+    Genre.create(name:"Country")
+    Genre.create(name:"Dance")
+    Genre.create(name:"Hip-Hop")
+    Genre.create(name:"Jazz")
+    Genre.create(name:"Metal")
+    Genre.create(name:"Pop")
+    Genre.create(name:"R&B")
+    Genre.create(name:"Reggae")
+    Genre.create(name:"Rock")
+    Genre.create(name:"Alternative")  
+    Genre.create(name:"Soul")
+    Genre.create(name:"Punk")
+    Genre.create(name:"Electronic")
+    Genre.create(name:"Rock")
+    Genre.create(name:"Folk")
+    Genre.create(name:"Latin")
+    Genre.create(name:"Indie")
