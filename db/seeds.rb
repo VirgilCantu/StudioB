b3cb5ebbfad6917eaaf3fb811392da55e4952352
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-15 19:12:09 -0500

    added 5 users to seed data to be associated with producer role

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+#----------------------Artists--------------------#
+
 User.create(
     name: "Billie Eilish Pirate Baird O'Connell", 
     email: "billie@billieeilish.com", 
@@ -55,3 +58,60 @@ User.create(
     can_travel: true, 
     picture: "https://upload.wikimedia.org/wikipedia/commons/1/15/Drake_at_The_Carter_Effect_2017_%2836818935200%29_%28cropped%29.jpg",
     stage_name: "Drake" )
+
+#----------------------Producers--------------------#    
+
+User.create(
+    name: "Frederick Jay Rubin", 
+    email: "frederick@rickrubin.com", 
+    password: "rickrubin", 
+    location: "Long Beach, New York",
+    bio:,
+    can_travel: false, 
+    picture: "https://upload.wikimedia.org/wikipedia/commons/4/43/RickRubinSept09.jpg",
+    stage_name: "Rick Rubin" )
+
+
+User.create(
+    name: "Khaled Mohamed Khaled", 
+    email: "khaled@djkhaled.com", 
+    password: "djkhaled", 
+    location: "Miami, Florida",
+    bio:,
+    can_travel: true, 
+    picture: "https://upload.wikimedia.org/wikipedia/commons/a/af/DJ_Khaled_2012.jpg",
+    stage_name: "DJ Khaled" )
+
+
+User.create(
+    name: "Timothy Zachary Mosley", 
+    email: "timothy@timbaland.com", 
+    password: "timbaland", 
+    location: "Miami, Florida",
+    bio:,
+    can_travel: false, 
+    picture: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Timbaland1292010.jpg",
+    stage_name: "Timbaland" )
+
+
+User.create(
+    name: "Michael Trent Reznor", 
+    email: "michael@trentreznor.com", 
+    password: "trentreznor", 
+    location: "Cleveland, Ohio",
+    bio:,
+    can_travel: true, 
+    picture: "https://upload.wikimedia.org/wikipedia/commons/c/c9/Trent_Reznor-FEB2008_%28cropped%2C_but_higher_quality%29_%282%29.jpg",
+    stage_name: "Trent Reznor" )
+
+User.create(
+    name: "Benjamin Joseph Levin", 
+    email: "benjamin@bennyblanco.com", 
+    password: "bennyblanco", 
+    location: "New York City, New York",
+    bio:,
+    can_travel: false, 
+    picture: "https://upload.wikimedia.org/wikipedia/commons/c/c9/Trent_Reznor-FEB2008_%28cropped%2C_but_higher_quality%29_%282%29.jpg",
+    stage_name: "Benny Blanco" )
+
+
