e063816d6c0a65ab59218e543c54d042c6f1d697
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-16 23:48:24 -0500

    added new and create actions to UsersController, added user_params method to sanitize form inputs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :name,
+            :email,
+            :password,
+            :location,
+            :bio,
+            :can_travel,
+            :picture,
+            :stage_name
+        )
+    end
+    
 end
