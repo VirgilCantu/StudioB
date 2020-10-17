611472d5e4739b9531409d272732466a758cfcd9
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-17 16:05:41 -0500

    added show, index, new, create, edit, and update actions to StudiosController, set up strong params

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,2 +1,42 @@
 class StudiosController < ApplicationController
+
+    def show
+        @studio = Studio.find_by(id: params[:id])
+    end
+
+    def index
+        @studios = Studio.all.sort_by {|studio| studio.name}
+    end
+    
+    def new
+        @studio = Studio.new
+    end
+
+    def create
+        @studio = Studio.create(studio_params)
+    end
+
+    def edit
+        @studio = Studio.find_by(id: params[:id])
+    end
+
+    def update
+        @studio = Studio.find_by(id: params[:id])
+        @studio.update(studio_params)
+        redirect_to studio_path(@studio)
+    end
+
+
+    private
+
+    def studio_params
+        params.require(:studio).permit(
+        :name,
+        :location,
+        :hourly_rate,
+        :features,
+        :description,
+        :picture
+        )
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
         redirect_to user_path(@user)
     end
 
+
     private
 
     def user_params
