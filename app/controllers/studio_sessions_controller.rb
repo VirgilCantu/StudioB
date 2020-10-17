db65d147d50a43bb529976d276014ff258a7d9a5
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-17 18:03:03 -0500

    added show, index, new, create, edit and update actions to StudioSessionsController, added strong params

--- app/controllers/studio_sessions_controller.rb
+++ app/controllers/studio_sessions_controller.rb
@@ -1,2 +1,42 @@
 class StudioSessionsController < ApplicationController
+
+    def show
+        @studio_session = StudioSession.find_by(id: params[:id])
+    end
+
+    def index
+        @studio_sessions = StudioSession.all
+    end
+    
+    def new
+        @studio_session = StudioSession.new
+    end
+
+    def create
+        @studio_session = StudioSession.create(studio_session_params)
+        redirect_to studio_session_path(@studio_session)
+    end
+
+    def edit
+        @studio_session = StudioSession.find_by(id: params[:id])
+    end
+
+    def update
+        @studio_session = StudioSession.find_by(id: params[:id])
+        @studio_session.update(studio_session_params)
+        redirect_to studio_session_path(@studio_session)
+    end
+
+
+    private
+
+    def studio_session_params
+        params.require(:studio_session).permit(
+        :session_time,
+        :duration,
+        :special_req,
+        :cost,
+        :studio_id
+        )
+    end
 end
