ae9bff4685268d853539b53a81907d76e7b036a8
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-20 14:18:11 -0500

    added conditional statment to create and update actoins in StudioSessions controller in order to trigger validation errors to be used in views

--- app/controllers/studio_sessions_controller.rb
+++ app/controllers/studio_sessions_controller.rb
@@ -13,10 +13,14 @@ class StudioSessionsController < ApplicationController
     end
 
     def create
-        @studio_session = StudioSession.create(studio_session_params)
+        @studio_session = StudioSession.new(studio_session_params)
         @studio_session.cost = params[:studio_session][:duration].to_i*@studio_session.studio.hourly_rate
-        @studio_session.save
-        redirect_to studio_session_path(@studio_session)
+
+        if @studio_session.save
+            redirect_to studio_session_path(@studio_session)
+        else
+            render :new
+        end
     end
 
     def edit
@@ -25,8 +29,13 @@ class StudioSessionsController < ApplicationController
 
     def update
         @studio_session = StudioSession.find_by(id: params[:id])
-        @studio_session.update(studio_session_params)
-        redirect_to studio_session_path(@studio_session)
+        @studio_session.cost = params[:studio_session][:duration].to_i*@studio_session.studio.hourly_rate
+
+        if @studio_session.update(studio_session_params)
+            redirect_to studio_session_path(@studio_session)
+        else
+            render :edit
+        end
     end
 
     def destroy
