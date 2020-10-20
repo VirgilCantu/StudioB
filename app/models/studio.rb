4812741088fd5c4903a9d8746eff44e3d828d5c3
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-19 21:22:40 -0500

    added validations to Studio model for presence of name, location, and hourly_rate, as well as numericality for hourly_rate

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,11 +1,13 @@
 class Studio < ApplicationRecord
 
     has_many :equipments
-
     has_many :studio_sessions
-
     has_many :user_sessions, through: :studio_sessions
-    
     has_many :users, through: :user_sessions
-    
+
+    validates :name, presence: true, uniqueness: true
+    validates :location, presence: true
+    validates :hourly_rate, presence: true
+    validates :hourly_rate, numericality: { greater_than_or_equal_to: 100 }
+
 end
