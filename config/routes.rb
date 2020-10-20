a1bcfcd741b79f83b1b2dd1c506abaf564c41c70
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-20 16:28:15 -0500

    added root route in routes.rb with matching controller action 'home' in ApplicationController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'application#home'
+
   resources :studios
   resources :studio_sessions
   resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
