184b02ec51b3db68ee5cd783f2158249cac44150
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-20 14:05:02 -0500

    added StudioSession validations for session_time, duration, studio_id and users

--- app/models/studio_session.rb
+++ app/models/studio_session.rb
@@ -3,4 +3,10 @@ class StudioSession < ApplicationRecord
   
   has_many :user_sessions
   has_many :users, through: :user_sessions
+
+  validates :session_time, uniqueness: true
+  validates :duration, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :studio_id, presence: true
+  validates :users, presence: true
+
 end
