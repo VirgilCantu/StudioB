83ddef94c536e4f94b811b882bc706463cc52e9d
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-21 16:47:48 -0500

    implemented basic login logic within create action of SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
 
     def new
-        @user = User.new
     end
 
     def create
+        @user = User.find_by(email: params[:session][:email])
+        redirect_to login_path unless @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
     end
 
 end
