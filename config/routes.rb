9f3cc08e16b705cd84c9a4b29bc3e4e80aa9d540
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-22 16:40:49 -0500

    fixed syntax error with /logout route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/login' , to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  post 'logout', to: 'sessions#destroy'
+  post '/logout', to: 'sessions#destroy'
 
   resources :studios
   resources :studio_sessions
