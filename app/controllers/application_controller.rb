252d6b827756406efe7d7f43cad225bd4fe15490
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-22 16:39:25 -0500

    added logged_in? method and require_login private method to ApplicationController, made current_user and logged_in? available to the view using helper_method macro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
+    
+    helper_method :current_user, :logged_in?
 
     def home
     end
 
     def current_user
         @user = User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        current_user != nil
+    end
+
+    private
+
+    def require_login
+        redirect_to root_path unless session.include? :user_id
     end
     
 end
