ae7f037748b0f3faeb7b27f5ea8a181e736184af
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-22 15:46:03 -0500

    refactored create action to use try method with user variable, added redirect to User show view on succesful authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,21 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(email: params[:session][:email])
-        redirect_to login_path unless @user.authenticate(params[:session][:password])
-        session[:user_id] = @user.id
+        user = User.find_by(name: params[:session[:name])
+        user = user.try(:authenticate, params[:session][:password])
+        
+        return redirect_to login_path unless user
+        
+        session[:user_id] = user.id
+
+        redirect_to user_path(user)
     end
 
     def destroy
         session.delete :user_id
-        redirect_to "/"
+        redirect_to root_path
     end
+
+    
 
 end
