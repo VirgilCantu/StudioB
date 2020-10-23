bad0c8433372c0839e8efd0add741c2020e0b06a
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-23 17:09:48 -0500

    Added 'Add Studio' button in User show view, to be displayed only if the User that is being viewed is the current_user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'application#home'
 
+  #A studio can only be created if your Roles include studio manager, and if they do, you can create studios only that belong to you, no one else.
+  #You can only create a StudioSession as a studio manager Role, and if you do create StudioSessions, only studios you manage will be available to select from.
+
   get '/login' , to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
