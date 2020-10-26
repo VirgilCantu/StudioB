5b408d52e5ebc63b8e4023b5df29d940f10450a5
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-25 23:30:05 -0500

    added line to log user in once they have created an account in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render :new
