5bf5ef3a37b5632665c4f822dd84e25cf289ca18
VirgilCantu <cantu.sampsom@gmail.com>
2020-10-28 16:45:41 -0500

    modified flash notices in ApplicationController to be one line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,28 +16,24 @@ class ApplicationController < ActionController::Base
     private
 
     def require_login
-        redirect_to root_path unless session.include? :user_id
+        redirect_to root_path, notice: "This page requires you to login, please try again!" unless session.include? :user_id
     end
 
     def require_manager
-        flash[:notice] = "This page requires a Studio Manager role, please try again!"
-        redirect_to root_path unless current_user.roles.include?(Role.find(4))
+        redirect_to root_path, notice: "This page requires a Studio Manager role, please try again!" unless current_user.roles.include?(Role.find(4))
     end
 
     def require_current_user
-        flash[:notice] = "Cannot modify another account other than your own, please try again!"
-        redirect_to root_path unless current_user.id == params[:id].to_i
+        redirect_to root_path, notice: "Cannot modify another account other than your own, please try again!" unless current_user.id == params[:id].to_i
     end
 
     def require_managed_studio
-        flash[:notice] = "Cannot modify a studio that you do not manage, please try again!"
-        redirect_to root_path unless current_user.id == @studio.studio_manager_id
+        redirect_to root_path, notice: "Cannot modify a studio that you do not manage, please try again!" unless current_user.id == @studio.studio_manager_id
     end
 
     def studio_managed_by_user
         if params[:studio_id]
-            flash[:notice] = "Cannot modify a studio session at a studio that you do not manage, please try again!"
-            redirect_to root_path unless Studio.find_by(id: params[:studio_id]).studio_manager_id == current_user.id
+            redirect_to root_path, notice: "Cannot modify a studio session at a studio that you do not manage, please try again!" unless Studio.find_by(id: params[:studio_id]).studio_manager_id == current_user.id
         end
     end
     
